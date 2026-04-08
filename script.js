f156f98e99e80daada68481652ffd92c612d2e6d
Paras <dhankharparas3@gmail.com>
2026-04-08 21:28:58 +0530

    api fetching

--- script.js
+++ script.js
@@ -6,7 +6,38 @@ function showNews(articles) {
     
     <img 
       src="${articles[0].image || 'https://picsum.photos/800/400'}"
-      style="width:100%; height:100%; object-fit:cover; border-radius:25px; object-position: center "
+      style="width:100%; height:90%; object-fit:cover; border-radius:25px; "
+    >
+
+    <div style="
+      position:absolute;
+      bottom:0;
+      left:0;
+      width:100%;
+      padding:5px;
+      color:black;
+      border-radius:25px;
+    ">
+      <h2>${articles[0].title}</h2>
+    </div>
+
+    </div> `;
+    mainCard.onclick = () => {
+    window.open(articles[0].url, "_blank");
+};
+    // grid 2 ka working system
+
+    let cardGrid2 = document.getElementById("card-grid-2");
+
+    for(let i=1;i<=2;i++){
+        let article = articles[i]
+    
+    cardGrid2.innerHTML=`
+    <div style="position:relative; height:100%; width:100%;">
+    
+    <img 
+      src="${article.image || 'https://picsum.photos/800/400'}"
+      style="width:100%; height:80%; object-fit:cover; border-radius:25px; "
     >
 
     <div style="
@@ -15,17 +46,45 @@ function showNews(articles) {
       left:0;
       width:100%;
       padding:20px;
-      color:white;
+      color:black;
       border-radius:25px;
+
     ">
-      <h2>${articles[0].title}</h2>
+      <h2>${article.title}</h2>
     </div>
 
-  </div>
-`;}
+    </div> `;
+
+    }
 
 
-const apiKey = "4b274adf8393c4b4c4d2208843758760"
+    // grid 3 ka kaam 
+    let cardGrid3 = document.getElementById("card-grid-3");
+
+    for(let i =3; i < articles.length ; i+=1){
+        let article = articles[i]
+    
+    let div = document.createElement("div");    //.   crads bna rhe hai small ones sara data fetch krna ke liye
+    div.className = "card-3";                   //.   class given taki css add kr ske
+
+    div.innerHTML = `
+    <img 
+      src="${article.image || 'https://picsum.photos/300/200'}"
+      style="width:100%; height:70%; object-fit:cover; border-radius:20px;"
+    >
+    <h3 style="padding:10px;">${article.title}</h3>
+  `;
+   
+   div.onclick = () => {
+    window.open(article[i].url,"_blank");
+   };
+
+   cardGrid3.appendChild(div);
+    }
+}
+
+
+const apiKey = "475367f7354f3465d78babb6a2f6110e"
 async function getNews(category){
 
     let url = `https://gnews.io/api/v4/top-headlines?lang=en&max=10&apikey=${apiKey}`;
